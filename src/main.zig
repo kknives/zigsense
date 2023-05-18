c5f2c1c46282211f49bbca6c37ec2c3b23388209
Shashwat Ganesh <shashwatganesh@protonmail.com>
2023-05-18 22:16:45 +0530

    Remove placeholder code

--- src/main.zig
+++ src/main.zig
@@ -38,13 +38,6 @@ pub fn main() !void {
     // stdout is for the actual output of your application, for example if you
     // are implementing gzip, then only the compressed bytes should be sent to
     // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
-
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // don't forget to flush!
 }
 
 test "simple test" {
